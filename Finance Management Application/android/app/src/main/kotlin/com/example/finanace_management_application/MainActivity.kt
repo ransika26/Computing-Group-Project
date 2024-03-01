ceb96fe3276a533de49616bb8d8d1f39595ddcfa
Sahan Ransika Perera <121636993+ransika26@users.noreply.github.com>
2024-03-01 21:46:31 +0530

    Delete Finance Management Application directory

--- Finance Management Application/android/app/src/main/kotlin/com/example/finanace_management_application/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.finanace_management_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Finance Management Application/lib/src/constants/sizes.dart
+++
@@ -1,4 +0,0 @@
-
-const DefaultSize = 30.0;
-const SplashContainerSize = 30.0;
-const ButtonHeight = 15.0;
